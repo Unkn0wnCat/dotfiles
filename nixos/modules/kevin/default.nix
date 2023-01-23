f1861101a00435ba1cd83dbfbef6d67391277fa4
Kevin Kandlbinder <kevin@kevink.dev>
2023-01-23 12:57:37 +0100

    Update user groups

--- nixos/modules/kevin/default.nix
+++ nixos/modules/kevin/default.nix
@@ -82,7 +82,7 @@ in {
       users.users.kevin = {
         isNormalUser = true;
         description = "Kevin Kandlbinder";
-        extraGroups = [ "wheel" "docker" "dialout" "networkmanager" ]; 
+        extraGroups = [ "wheel" "docker" "dialout" "networkmanager" "floppy" "audio" "lp" "cdrom" "tape" "video" "render" ]; 
       };
       kevin.ssh.authorized.kevin.users = ["kevin" "root"];
     })
