b7685fe749969247dae505f9e7bf7a3b37c558c0
Kevin Kandlbinder <kevin@kevink.dev>
2023-02-27 14:16:18 +0000

    Add password as secret

--- nixos/modules/users.nix
+++ nixos/modules/users.nix
@@ -1,10 +1,19 @@
 {
   imports = [ ./ssh.nix ];
 
+  sops.secrets.password_kevin = {
+    neededForUsers = true;
+    sopsFile = ../shared/secrets/passwords.yaml;
+  };
+
+
+  users.mutableUsers = false;
+
   users.users.kevin = {
     isNormalUser = true;
     description = "Kevin Kandlbinder";
     extraGroups = [ "wheel" "docker" "dialout" "networkmanager" "floppy" "audio" "lp" "cdrom" "tape" "video" "render" ]; 
+    passwordFile = config.sops.secrets.password-kevin.path;
   };
   
   kevin.ssh.authorized.kevin.users = ["kevin" "root"];
