28865a062fe5d6096808056cb543987fa01d53cd
Kevin Kandlbinder <kevin@kevink.dev>
2023-05-13 20:43:26 +0200

    Add Docker IPv6 configuration

--- nixos/modules/server/docker.nix
+++ nixos/modules/server/docker.nix
@@ -1,6 +1,11 @@
 {
   virtualisation.docker = {
     enable = true;
-    autoPrune.enable = true;
+    autoPrune.enable = true; 
+
+    daemon.settings = {
+      ipv6 = true;
+      fixed-cidr-v6 = "fd00::/80";
+    };
   };
 }
