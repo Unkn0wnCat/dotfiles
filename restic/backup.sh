16e2943397bc3a43e0b2d90526a056d9af02abb7
Kevin Kandlbinder <kevin@kevink.dev>
2023-05-25 15:58:15 +0200

    Update backup script to use wrapped restic

--- restic/backup.sh
+++ restic/backup.sh
@@ -8,7 +8,7 @@ export RESTIC_PASSWORD_FILE=/var/run/secrets/restic/password
 export AWS_ACCESS_KEY_ID="$(cat /var/run/secrets/restic/aws_id)"
 export AWS_SECRET_ACCESS_KEY="$(cat /var/run/secrets/restic/aws_secret)"
 
-restic backup --iexclude-file ./backup-iexclude.list --exclude-file ./backup-exclude.list --files-from ./backup.list --exclude-if-present ".nobackup" --exclude-if-present ".git" --exclude-if-present ".nextcloudsync.log" --exclude-if-present ".owncloudsync.log" --tag nixos
-restic forget --prune --keep-last 10 --keep-daily 14 --keep-weekly 10 --keep-monthly 24 --keep-yearly 100
+/run/wrappers/bin/restic backup --iexclude-file ./backup-iexclude.list --exclude-file ./backup-exclude.list --files-from ./backup.list --exclude-if-present ".nobackup" --exclude-if-present ".git" --exclude-if-present ".nextcloudsync.log" --exclude-if-present ".owncloudsync.log" --tag nixos
+/run/wrappers/bin/restic forget --prune --keep-last 10 --keep-daily 14 --keep-weekly 10 --keep-monthly 24 --keep-yearly 100
 
 popd
