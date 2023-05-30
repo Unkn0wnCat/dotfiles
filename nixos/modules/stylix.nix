1816fb6fb5ad05cb92c637e9cb0b55eb6d3c9dc8
Kevin Kandlbinder <kevin@kevink.dev>
2023-05-30 14:17:44 +0200

    Add basic color scheme

--- nixos/modules/stylix.nix
+++ nixos/modules/stylix.nix
@@ -2,5 +2,6 @@
   stylix = {
     image = ../../assets/wallpapers/kevin-kandlbinder-u4wkyXFGUc4-unsplash.jpg;
     polarity = "dark";
+    base16Scheme = ../../colors/schemes/base16-vroom.yaml;
   };
 }
